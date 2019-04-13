24f6e0e031d918a212ba7c61fb1a6fead2f526a6
blacky <xblackypl@gmail.com>
2019-04-13 14:06:05 +0200

    Image data ready for algoritms, image display in ratarded form

--- TreeCrownDetection/MainWindow.xaml.cs
+++ TreeCrownDetection/MainWindow.xaml.cs
@@ -1,99 +1,182 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 using OSGeo.GDAL;
+using Image = System.Windows.Controls.Image;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
 
 namespace TreeCrownDetection
 {
+    public class RawImage
+    {
+        public RawImage(double[] rawImageData, int width, int height)
+        {
+            Width = width;
+            Height = height;
+            RawImageData = rawImageData;
+        }
+
+        public int Width { get; }
+        public int Height { get; }
+        public double[] RawImageData { get; }
+    }
+
     public partial class MainWindow : Window
     {
         private string _inputFilename;
-        public bool Complete = false;
+        private RawImage _loadedImage;
+        public bool Complete;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        public bool BandCountError { get; set; }
+
         private async void OpenFileButtonClick(object sender, RoutedEventArgs e)
         {
-            var dlg = new Microsoft.Win32.OpenFileDialog
+            var dlg = new OpenFileDialog
             {
                 DefaultExt = ".geoTIFF",
                 Filter =
                     "geoTIFF Files(*.geoTIFF)|*.gTIFF|TIFF Files (*.tiff)|*.tiff|TIF Files (*.tif)|*.tif"
             };
-            
+
             var result = dlg.ShowDialog();
             if (result != true) return;
 
             _inputFilename = dlg.FileName;
-          
-            var progress = new Progress<int>(percent =>
-            {
-                ProgressBar.Value = percent;
-            });
+
+            var progress = new Progress<int>(percent => { ProgressBar.Value = percent; });
 
             await ReadGeoTiffFiles(_inputFilename, progress);
+
+            if (!BandCountError)
+            {
+                var background = ToBitmap(_loadedImage.RawImageData, _loadedImage.Width, _loadedImage.Height);
+                var backgroundSource = Convert(background);
+                var displayImage = new Image {Source = backgroundSource};
+                ImageDock.Children.Clear();
+                ImageDock.Children.Add(displayImage);
+                ProgressBar.Value = 100;
+            }
+            else
+            {
+                MessageBox.Show(@"Input error, please provide single band raster image only..", "Tree Crow Detection",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            if (Complete.Equals(true))
+            {
+                ProgressBar.Value = 0;
+            }
         }
 
         public async Task ReadGeoTiffFiles(string path, IProgress<int> progress)
         {
             GdalConfiguration.ConfigureGdal();
-            if (null == path)
+            if (null == path) return;
+
+            await Task.Run(() =>
             {
-                return;
-            }
-       
-            await Task.Run((() =>
-                    {
-                        progress.Report(10);
-                        var ds = Gdal.Open(path, Access.GA_ReadOnly);
-                        Console.WriteLine(@"Raster data-set parameters:");
-                        Console.WriteLine(@"  Projection: " + ds.GetProjectionRef());
-                        Console.WriteLine(@"  RasterCount: " + ds.RasterCount);
-                        Console.WriteLine(@"  RasterSize (" + ds.RasterXSize + @"," + ds.RasterYSize + @")");
+                BandCountError = false;
+                progress.Report(10);
+                var ds = Gdal.Open(path, Access.GA_ReadOnly);
+                Console.WriteLine(@"Raster data-set parameters:");
+                Console.WriteLine(@"  Projection: " + ds.GetProjectionRef());
+                Console.WriteLine(@"  RasterCount: " + ds.RasterCount);
+                Console.WriteLine(@"  RasterSize (" + ds.RasterXSize + @"," + ds.RasterYSize + @")");
 
-                        progress.Report(20);
-                        var bandCount = ds.RasterCount;
-                        if (bandCount > 1)
-                        {
-                            Console.WriteLine(@"Input error, please provide single band raster image only..");
-                            System.Environment.Exit(-1);
-                        }
-                        var rasterCols = ds.RasterXSize;
-                        var rasterRows = ds.RasterYSize;
+                progress.Report(20);
+                var bandCount = ds.RasterCount;
+                if (bandCount > 1)
+                {
+                    BandCountError = true;
+                    Complete = true;
+                    return;
+                }
 
-                        var transformation = new double[6];
-                        ds.GetGeoTransform(transformation);
+                var rasterCols = ds.RasterXSize;
+                var rasterRows = ds.RasterYSize;
 
-                        var originX = transformation[0];
-                        var originY = transformation[3];
-                        var pixelWidth = transformation[1];
-                        var pixelHeight = transformation[5];
-                        progress.Report(60);
-                        var band = ds.GetRasterBand(1);
-                        var rasterWidth = rasterCols;
-                        var rasterHeight = rasterRows;
-                        progress.Report(80);
-                        var rasterValues = new double[rasterWidth * rasterHeight];
-                        band.ReadRaster(0, 0, rasterWidth, rasterHeight, rasterValues, rasterWidth, rasterHeight, 0, 0);
-                        progress.Report(100);
-                        Complete = true;
-                    }
-                ));
+                var transformation = new double[6];
+                ds.GetGeoTransform(transformation);
+
+                progress.Report(30);
+                var band = ds.GetRasterBand(1);
+                var rasterWidth = rasterCols;
+                var rasterHeight = rasterRows;
+                progress.Report(50);
+                var rasterValues = new double[rasterWidth * rasterHeight];
+                band.ReadRaster(0, 0, rasterWidth, rasterHeight, rasterValues, rasterWidth, rasterHeight, 0, 0);
+                progress.Report(70);
+                _loadedImage = new RawImage(rasterValues, rasterWidth, rasterHeight);
+                Complete = true;
+                progress.Report(90);
+            });
         }
+
+
+        public BitmapSource Convert(Bitmap bitmap)
+        {
+            var bitmapData = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+            var bitmapSource = BitmapSource.Create(
+                bitmapData.Width, bitmapData.Height,
+                bitmap.HorizontalResolution, bitmap.VerticalResolution,
+                PixelFormats.Bgr24, null,
+                bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
+
+            bitmap.UnlockBits(bitmapData);
+            return bitmapSource;
+        }
+
+        public struct ColorARGB
+        {
+            public byte B;
+            public byte G;
+            public byte R;
+            public byte A;
+        }
+
+        private static unsafe Bitmap ToBitmap(IReadOnlyList<double> rawImage, int width, int height)
+        {
+            var image = new Bitmap(width, height);
+            var bitmapData = image.LockBits(
+                new Rectangle(0, 0, width, height),
+                ImageLockMode.ReadWrite,
+                PixelFormat.Format32bppArgb
+            );
+
+            var startingPosition = (ColorARGB*) bitmapData.Scan0;
+
+            for (var i = 0; i < height; i++)
+            for (var j = 0; j < width; j++)
+            {
+                var color = rawImage[i * width + j];
+                var rgb = (byte) (color * 255);
+
+                var position = startingPosition + j + i * width;
+                position->A = 255;
+                position->R = rgb;
+                position->G = rgb;
+                position->B = rgb;
+            }
+
+            image.UnlockBits(bitmapData);
+            return image;
+        }
+
 
     }
 }
